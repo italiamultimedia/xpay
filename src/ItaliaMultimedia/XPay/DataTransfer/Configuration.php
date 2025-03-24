4639f9107f0c1766e95005c9b8fdb04c724f1728
Radu Maier <ariademur@webserv.co>
2025-03-24 18:19:37 +0100

    Add constant types.

--- src/ItaliaMultimedia/XPay/DataTransfer/Configuration.php
+++ src/ItaliaMultimedia/XPay/DataTransfer/Configuration.php
@@ -6,27 +6,27 @@ namespace ItaliaMultimedia\XPay\DataTransfer;
 
 final class Configuration
 {
-    public const API_URL_PRODUCTION = 'https://ecommerce.nexi.it/';
+    public const string API_URL_PRODUCTION = 'https://ecommerce.nexi.it/';
 
-    public const API_URL_TEST = 'https://int-ecommerce.nexi.it/';
+    public const string API_URL_TEST = 'https://int-ecommerce.nexi.it/';
 
     /**
      * Used in simple payments, and recurring payments (initial).
      */
-    public const CURRENCY = 'EUR';
+    public const string CURRENCY = 'EUR';
 
     /**
      * Used in recurring payments (subsequent).
      * https://ecommerce.nexi.it/specifiche-tecniche/pagamentoricorrente/pagamentisuccessivi.html
      * Unico valore ammesso: 978 (Euro)
      */
-    public const CURRENCY_CODE = 978;
+    public const int CURRENCY_CODE = 978;
 
-    public const ENVIRONMENT_PRODUCTION = 'production';
+    public const string ENVIRONMENT_PRODUCTION = 'production';
 
-    public const ENVIRONMENT_TEST = 'test';
+    public const string ENVIRONMENT_TEST = 'test';
 
-    public const RECURRING_PAYMENT_SUBSEQUENT_API_ENDPOINT = 'ecomm/api/recurring/pagamentoRicorrente';
+    public const string RECURRING_PAYMENT_SUBSEQUENT_API_ENDPOINT = 'ecomm/api/recurring/pagamentoRicorrente';
 
-    public const SIMPLE_PAYMENT_API_ENDPOINT = 'ecomm/ecomm/DispatcherServlet';
+    public const string SIMPLE_PAYMENT_API_ENDPOINT = 'ecomm/ecomm/DispatcherServlet';
 }
